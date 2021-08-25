e556b70f70afcfbe79c69dbd7970a554ab5350fc
TMN <t.martin@adsoftware.fr>
2021-08-25 14:12:28 +0200

    Updates

---
+++ precache-manifest.ea5d251ff5f9841c4775b2b0d3719dfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05dcffd45e6c3abcc08e389e27815f49",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2d7cd1342250954a3dc",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "8dd622df4991b83d4969",
+    "url": "./static/js/2.cb76fc38.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cb76fc38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d7cd1342250954a3dc",
+    "url": "./static/js/main.2f05030e.chunk.js"
+  },
+  {
+    "revision": "d466afdfdc668be0d995",
+    "url": "./static/js/runtime-main.1db1f701.js"
+  }
+]);
