f7cfb7634dee0295ca25327f8ed590984ba21c44
TMN <t.martin@adsoftware.fr>
2021-08-25 11:12:46 +0200

    init

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
 
-import { ExampleComponent } from 'qs-filter'
 import 'qs-filter/dist/index.css'
+import { useFilterFromQueryParams } from 'qs-filter'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  type User = {
+    id: number
+    isActive: boolean
+    isFinalized: boolean
+  }
+  const users: User[] = [
+    {
+      id: 1,
+      isActive: true,
+      isFinalized: false
+    },
+    {
+      id: 2,
+      isActive: false,
+      isFinalized: true
+    }
+  ]
+  const filteredUsers = useFilterFromQueryParams(users)
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <div>
+        <h1>Examples</h1>
+        <a href='http://localhost:3000?isActive=true'>
+          http://localhost:3000?isActive=true
+        </a>
+        <br />
+        <a href='http://localhost:3000?isFinalized=true'>
+          http://localhost:3000?isFinalized=true
+        </a>
+      </div>
+      {filteredUsers.map((user) => (
+        <>
+          <p>Id : {user.id}</p>
+          <p>isActive : {user.isActive.toString()}</p>
+          <p>isFinalized : {user.isFinalized.toString()}</p>
+        </>
+      ))}
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,33 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import * as qs from 'query-string'
 
-interface Props {
-  text: string
+interface Item {
+  [key: string]: any
 }
+/**
+ * @description Attention, pour le moment seul les query params de type boolean sont pris en compte
+ * @param items array d'items à filtrer
+ * @returns items filtrés selon les query params
+ * @example const filteredUsers = useFilterFromQueryParams(users)
+ */
+export const useFilterFromQueryParams = (items: Item[]): Item[] => {
+  if (!items) return []
+  const queryParams = qs.parse(window.location.search, {
+    parseBooleans: true,
+    arrayFormat: 'comma'
+  })
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  if (!Object.values(queryParams)?.length) return items
+
+  return items.filter((item) => {
+    const filters = Object.entries(queryParams).map(([key, value]) => {
+      if (typeof value !== 'boolean') {
+        console.error(
+          `⚠️ Type "${typeof value}" is not supported by useFilterFromQueryParams yet. This filter will be ignored`
+        )
+        return true
+      }
+      return item[key] === value
+    })
+    return !filters.includes(false)
+  })
 }
