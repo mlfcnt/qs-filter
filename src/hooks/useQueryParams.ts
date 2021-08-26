0a5e4b6270b9902980c0cc024e89381ec4f05836
TMN <t.martin@adsoftware.fr>
2021-08-26 11:00:17 +0200

    logs

--- src/hooks/useQueryParams.ts
+++ src/hooks/useQueryParams.ts
@@ -3,6 +3,7 @@ import { useMemo } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export const useQueryParams = (options?: qs.ParseOptions) => {
+  console.log('inside useQueryParams')
   const defaultOptions: qs.ParseOptions = {
     parseBooleans: true,
     arrayFormat: 'comma'
@@ -11,10 +12,13 @@ export const useQueryParams = (options?: qs.ParseOptions) => {
     ...defaultOptions,
     ...options
   }
+  console.log('🚀 ~ useQueryParams ~ finalOptions', finalOptions)
+
   const { search } = useLocation()
   const queryParams = useMemo(
     () => qs.parse(search, finalOptions),
     [search, finalOptions]
   )
+  console.log('🚀 ~ useQueryParams ~ queryParams', queryParams)
   return queryParams
 }
