57b5cafacbafaab857bbbc399730d87aec392c57
TMN <t.martin@adsoftware.fr>
2021-08-26 09:56:16 +0200

    fix: test

--- src/hooks/useFilterFromQueryParams.tsx
+++ src/hooks/useFilterFromQueryParams.tsx
@@ -1,4 +1,5 @@
 import * as qs from 'query-string'
+import { useEffect, useState } from 'react'
 
 interface Item {
   [key: string]: any
@@ -9,25 +10,31 @@ interface Item {
  * @returns items filtered according to query params
  * @example const filteredUsers = useFilterFromQueryParams(users)
  */
-export const useFilterFromQueryParams = (items?: Item[]): Item[] => {
+export const useFilterFromQueryParams = (items: Item[] = []): Item[] => {
+  const [filteredItems, setFilteredItems] = useState<Item[]>(items)
   if (!items) return []
   const queryParams = qs.parse(window.location.search, {
     parseBooleans: true,
     arrayFormat: 'comma'
   })
+  useEffect(() => {
+    setFilteredItems(filterItems())
+  }, [queryParams])
 
-  if (!Object.values(queryParams)?.length) return items
-
-  return items.filter((item) => {
-    const filters = Object.entries(queryParams).map(([key, value]) => {
-      if (typeof value !== 'boolean') {
-        console.error(
-          `⚠️ Type "${typeof value}" is not supported by useFilterFromQueryParams yet. This filter will be ignored`
-        )
-        return true
-      }
-      return item[key] === value
+  const filterItems = () => {
+    if (!Object.values(queryParams)?.length) return items
+    return items.filter((item) => {
+      const filters = Object.entries(queryParams).map(([key, value]) => {
+        if (typeof value !== 'boolean') {
+          console.error(
+            `⚠️ Type "${typeof value}" is not supported by useFilterFromQueryParams yet. This filter will be ignored`
+          )
+          return true
+        }
+        return item[key] === value
+      })
+      return !filters.includes(false)
     })
-    return !filters.includes(false)
-  })
+  }
+  return filteredItems
 }
